290f35bd28ba2c91e5f97a996a22e5eebb0cba35
Lawrence Jones <lawrencejones1234@icloud.com>
2024-03-16 01:14:42 -0500

    oop rspec, modified

--- spec/oop_spec.rb
+++ spec/oop_spec.rb
@@ -1,25 +1,25 @@
 require 'oop'
 
 RSpec.describe Car do
-    let(:my_car) { Car.new }
+    let(:my_car) { Car.new('Toyota', 'Camry', 'Blue', 25000) }
 
     describe "#set_price" do
     it "sets the price" do
-        my_car.set_price(90300.00)
-        expect(my_car.price).to eq(90300.00)
+        my_car.set_price(25000)
+        expect(my_car.price).to eq(25000)
     end
-end
+    end
     
     describe "#paint" do
     it "gives the car a color" do
-        my_car.paint('black')
-        expect(my_car.color).to eq('black')
+        my_car.paint('Blue')
+        expect(my_car.color).to eq('Blue')
     end
     end
 
     describe "#show_car_info" do
     it "lists the attributes of the car" do
-        expect(my_car.show_car_info).to output.to.stdout
+        expect { my_car.show_car_info }.to output.to_stdout
     end
     end
 
